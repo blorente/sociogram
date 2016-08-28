22fbec6feaa4c6c65db1b41f74ff3261d8dea045
Borja Lorente <blorente@ucm.es>
2016-08-28 19:22:18 +0200

    Add chai and dummy tests

---
+++ tests/dummy.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+
+describe('Dummy tests', function() {
+	it('One plus one equals two', function () {
+		expect(1 + 1).to.equal(2);
+	})
+});
